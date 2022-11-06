bdc6009baafa88ec7f04db6d7bffeb3062585225
Jordi <jordilop@gmail.com>
2022-11-06 21:13:45 +0100

    Exercici 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [quotation, setQuotation] = useState(0);
+  const [web, setWeb] = useState(0);
+  const [seo, setSeo] = useState(0);
+  const [ads, setAds] = useState(0);
+
+  useEffect(() => {
+    setQuotation(web + seo + ads)
+  }, [web, seo, ads]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form>
+        <p>Que vols fer?</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="checkbox" name="web" value="500" onChange={event => setWeb(event.target.checked ? Number(event.target.value) : 0)} />
+          Una página Web (500€)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <input type="checkbox" name="seo" value="300" onChange={event => setSeo(event.target.checked ? Number(event.target.value) : 0)} />
+          Una consultoria SEO (300€)
+        </p>
+        <p>
+          <input type="checkbox" name="ads" value="200" onChange={event => setAds(event.target.checked ? Number(event.target.value) : 0)} />
+          Una campanya de Google Ads (200€)
+        </p>
+      </form>
+      <p>Preu: {quotation}€</p>
     </div>
   );
 }
